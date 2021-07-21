8c4d15d989a48df257d99c407f015801c894fb1a
unknown <fabiofsilva92@gmail.com>
2021-07-21 20:33:09 -0300

    first commit api test

---
+++ src/test/java/one/digitalinnovation/fabiofsilva92/personAPI/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.fabiofsilva92.personAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
